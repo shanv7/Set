531bcf54f66b047a89f96a18aef2397b4290cf58
shanv7 <32124811+shanv7@users.noreply.github.com>
2017-11-11 17:31:20 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import numpy as np
 from extract import extract
 from color import color
 
-card = extract('set.jpg', 9)
-color(card[0])
+COL = 0       # R=0, G=1, P=2
+NUM = 1       # 1=1, 2=2, 3=3
+SHADE = 2     # E=0, S=1, F=2
+SHAPE = 3     # O=0, D=1, S=2
+NCARDS = 9
+
+res = np.empty([9, 4], dtype=np.uint8)
+
+card = extract('set.jpg', NCARDS)
+
+for i in range(NCARDS):
+    res[i, COL] = color(card[i])
+
+
+
+print(res)
