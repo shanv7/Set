9f82e8fb22ff3f198964146a01e2d3ebf466df81
shanv7 <32124811+shanv7@users.noreply.github.com>
2017-11-11 09:57:49 +0530

    Add files via upload

--- extract.py
+++ extract.py
@@ -1,41 +1,37 @@
-#!/usr/bin/python
-
 import cv2
 import numpy as np
-
-numcards = 9
-ids = np.zeros(4 * numcards)
+import imutils
 
 
-im = cv2.imread('set.jpg')
-gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (1, 1), 1000)
-flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
+def extract(image, n):
+    card = []
+    image = str(image)
+    im = cv2.imread(image)
+    im = imutils.resize(im, height=1000)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (1, 1), 1000)
+    flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
 
-imc, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)[:numcards]
+    imc, contour, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contour = sorted(contour, key=cv2.contourArea, reverse=True)[:n]
 
-for i in range(numcards):
-    card = contours[i]
-
-    # imc2, c2, h2, = cv2.findContours(card, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # c2 = sorted(contours, key=cv2.contourArea, reverse=True)[:numcards]
-    # for j in c2:
-        
-    peri = cv2.arcLength(card, True)
-    approx = np.array([cv2.approxPolyDP(card, 0.02 * peri, True)], np.float32)
-    approx = approx.reshape(4, 2)
-    rect = cv2.minAreaRect(contours[2])
-    r = cv2.boxPoints(rect)
-    h = np.array([[0, 0], [399, 0], [399, 599], [0, 599]], np.float32)
-    transform = cv2.getPerspectiveTransform(approx, h)
-    warp = cv2.warpPerspective(im, transform, (400, 600))
-    cv2.namedWindow(str(i+1), cv2.WINDOW_NORMAL)
-    cv2.imshow(str(i+1), warp)
-
-cv2.waitKey(0)
-
-
-# cv2.namedWindow('test', cv2.WINDOW_NORMAL)
-# cv2.imshow('test', im2)
-# cv2.waitKey(0)
+    for i in range(n):
+        peri = cv2.arcLength(contour[i], True)
+        approx = cv2.approxPolyDP(contour[i], 0.02 * peri, True)
+        pts = approx.reshape(4, 2)
+        rect = np.zeros((4, 2), dtype="float32")
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        dst = np.array([
+            [0, 0],
+            [399, 0],
+            [399, 399],
+            [0, 399]], dtype="float32")
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warp = cv2.warpPerspective(im, M, (400, 400))
+        card.append(warp)
+    return card
---
+++ main.py
@@ -0,0 +1,5 @@
+from extract import extract
+from color import color
+
+card = extract('set.jpg', 9)
+color(card[0])
